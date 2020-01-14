688163b537aeccf7abd12c84eff4412f02e17dae
母猪赛貂蝉 <332902234@qq.com>
2020-01-14 21:46:37 +0800

    Update

--- script/jrrb.js
+++ script/jrrb.js
@@ -1,10 +1,12 @@
-//今日热榜 crate by makexp 2020.01.14
-//^https:\/\/api\.tophub\.today\/account\/sync* url script-response-body jrrb.js
-
-let obj = JSON.parse($response.body);
-obj["data"]={
-	"vip_expired": "18630733746",
-	"is_vip_now": true,
-	"is_vip": "1"
-	}
+var obj = JSON.parse($response.body);
+ obj={
+ "data": {
+  "is_vip": "1",
+  "vip_expired": "4092599349",
+  "is_vip_now": true
+ },
+ "error": false,
+ "status": 200
+};
 $done({body: JSON.stringify(obj)});
+//
